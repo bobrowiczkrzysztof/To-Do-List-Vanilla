1bf6ec19ecd0043d24ed13e8547bc500322d0b18
bobrowiczkrzysztof <69210846+bobrowiczkrzysztof@users.noreply.github.com>
2022-01-03 13:45:22 +0100

    Added edit function

--- script.js
+++ script.js
@@ -4,22 +4,29 @@ const tasks = document.querySelector('.tasks');
 
 addButton.addEventListener('click', function () {
   if (input.value.length == 0) {
-    alert('What the hell are you thinking about?');
+    alert('Please fill out the task');
   } else {
     tasks.innerHTML += `
     <div class="task">
     
-    <span class="task-text">${input.value}</span> <i class="far fa-trash-alt remove"></i>
-   
+    <span class="task-text">
+    <input type="text"
+    value="${input.value}" />
+    </span>
+    <div class="buttons"> 
+    <i class="far fa-trash-alt remove"></i>
+    </div>
     </div>`;
     input.value = '';
   }
-  let removeBtn = document.querySelectorAll('.remove');
+  const removeBtn = document.querySelectorAll('.remove');
+  const editButton = document.querySelectorAll('.edit-button');
+  const taskText = document.querySelectorAll('.task-text');
   console.log(removeBtn);
 
   removeBtn.forEach(btn => {
     btn.addEventListener('click', function () {
-      let parent = this.parentNode;
+      let parent = this.parentNode.parentNode;
       parent.remove();
     });
   });
